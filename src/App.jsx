453fa701c38820557a156b77818793879cff98fc
AvinashAvi00 <avinaskumaravi003@gmail.con>
2024-04-20 11:08:45 +0530

    Added footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
 
 function App() {
-
   return (
     <>
-    hello world 
+      <Routes>
+        {/* Define your routes here */}
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* Add more routes as needed */}
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
